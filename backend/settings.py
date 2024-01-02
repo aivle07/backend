a321aeb60662363f38205d0ddceb0797b5cddab5
woosang <dnehtks123@naver.com>
2024-01-02 17:39:42 +0900

    db setting 수정

--- backend/settings.py
+++ backend/settings.py
@@ -149,13 +149,7 @@ ASGI_APPLICATION = "backend.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = db_settings.DATABASES
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = db_settings.DATABASES
 
 
 CHANNEL_LAYERS = {
