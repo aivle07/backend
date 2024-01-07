d37167248b8ba8f3000cf80face06e2920ed2380
jsmount <park20312@naver.com>
2024-01-08 01:48:00 +0900

    기업 평가값 거의 완성

--- summary/gold/langchain_gold.py
+++ summary/gold/langchain_gold.py
@@ -31,13 +31,13 @@ def get_gold_data_agent():
     return agent
 
 def get_gold_data():
-    df_gold = fdr.DataReader('GC=F',"2023-12-01","2024-01-01")
+    df_gold = fdr.DataReader('GC=F',"2023-12-01")
     
     nan_rows = df_gold[df_gold.isna().any(axis=1)].index
 
     df_gold.dropna(inplace=True)
     
-    df_usa = fdr.DataReader('USD/KRW',"2023-12-01","2024-01-01")
+    df_usa = fdr.DataReader('USD/KRW',"2023-12-01")
     df_usa.drop(nan_rows, inplace=True)
     
     df_gold['Open'] = df_gold['Open'] / 31.1034768 * df_usa['Open'] / 0.9999
@@ -50,7 +50,7 @@ def get_gold_data():
     df_gold['Year'] = df_gold['Date'].dt.year
     df_gold['Month'] = df_gold['Date'].dt.month
     df_gold['Day'] = df_gold['Date'].dt.day
-    df_gold.drop(columns=['Date'])
+    # df_gold.drop(columns=['Date'])
     return df_gold
 
 # 금 챗봇 결과 생성
